66b8988cf908c875f1b8b4778ac09699ae513490
ChengHoward <18071131140@sina.cn>
2023-12-28 15:01:39 +0800

    ✨新增`CloudbypassProxy`用于生成穿云代理

--- src/cloudbypass/proxy.py
+++ src/cloudbypass/proxy.py
@@ -10,10 +10,9 @@ class CloudbypassProxy:
         """
         :param auth: format: 12345678-res:password
         """
-        auth = self.check_auth(auth)
-        self.__username, self.__password = auth.split(':')
+        self.__username, self.__password = self.check_auth(auth)
         self.__region = kwargs.get('region')
-        self.__expire = kwargs.get('expire', -1)
+        self.__expire = kwargs.get('expire', 0)
         self.__session_id = None
 
     def set_expire(self, expire):
@@ -29,9 +28,7 @@ class CloudbypassProxy:
         """
         :return:
         """
-        self.__expire = -1
-        self.__session_id = None
-        return self
+        return self.set_expire(0)
 
     def set_gateway(self, gateway):
         """
@@ -96,16 +93,7 @@ class CloudbypassProxy:
         if auth is None or re.match(r'^\w+-(res|dat):\w+$', auth) is None:
             raise ValueError('Invalid auth format')
 
-        return auth
-
-    @staticmethod
-    def random_session():
-        """
-        :return:
-        """
-        return "".join(
-            random.choices('0123456789abcdefghijklmnopqrstuvwxyz', k=11)
-        )
+        return auth.split(':')
 
     @property
     def session_id(self):
@@ -114,7 +102,9 @@ class CloudbypassProxy:
         """
         # __session_id
         if self.__session_id is None:
-            self.__session_id = self.random_session()
+            self.__session_id = "".join(
+                random.choices('0123456789abcdefghijklmnopqrstuvwxyz', k=11)
+            )
 
         return self.__session_id
 
@@ -129,7 +119,7 @@ class CloudbypassProxy:
 
         if expire is not None:
             for time, unit in [(60, 's'), (60, 'm'), (24, 'h'), (999, 'd')]:
-                if not (expire >= time and not expire % time):
+                if expire < time or expire % time:
                     options.append(f'{self.session_id}-{expire}{unit}')
                     break
                 expire //= time
@@ -169,10 +159,9 @@ class CloudbypassProxy:
             'expire': self.__expire,
         })
 
-    def new(self):
+    def copy(self):
         """
         Copy a new proxy
         :return:
         """
         return self.__copy__()
-
